7fc5815f0cd5a1c0f2c1c09579d059f0a15d743c
laohe <laohe@163.com>
2019-06-24 21:05:42 +0800

    axios发送请求获得人机验证

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from '@/App.vue';
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './styles/index.less';
+
+import '@/styles/index.less';
 
 import router from './router';
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="login-wrap">
     <el-form class="login-form" ref="form" :model="sizeForm"  size="mini">
+      <img src="../../assets/logo_index.png" alt="">
       <el-form-item >
-        <el-input v-model="sizeForm.mobile" placeholder="手机号"></el-input>
+        <el-input  v-model="sizeForm.mobile" placeholder="手机号"></el-input>
       </el-form-item>
       <el-form-item >
-        <el-input v-model="sizeForm.code" placeholder="验证码"></el-input>
+        <el-input class="aaa" v-model="sizeForm.code" placeholder="验证码"></el-input>
+         <el-button class="bbb" @click="handleSendCode">发送验证码</el-button>
       </el-form-item>
+      <input  type="radio"> 我已阅读用户协议和隐私条款
       <el-form-item>
-        <el-button type="primary" @click="onSubmit" >登录</el-button>
-        <el-button>取消</el-button>
+
+        <el-button class="ccc" type="primary" @click="onSubmit" >登录</el-button>
+        <el-button class="ccc">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'AppLogin',
   data () {
@@ -29,6 +35,16 @@ export default {
   methods: {
     onSubmit () {
       console.log('submit!');
+    },
+    handleSendCode () {
+      const { mobile } = this.sizeForm;
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+
+      }).then((res) => {
+        console.log(res.data);
+      });
     }
   }
 };
@@ -37,12 +53,35 @@ export default {
 <style lang="less" scoped>
   .login-wrap {
       height:100%;
-      background:pink;
+      background:url(../../assets/login_bg.jpg) no-repeat 100% 100% ;
       display:flex;
       justify-content:center;
       align-items:center;
       .login-form {
+          height: 230px;
+          width: 350px;
+          background: #fff;
+          border-radius:10px;
+          padding:10px;
 
+          img{
+              width: 200px;
+              margin-left: 20%;
+          }
+          .aaa{
+            width: 230px;
+          }
+          .bbb{
+            margin-left: 20px;
+          }
+          .bbb:hover{
+            color: aqua;
+
+          }
+          .ccc{
+            margin-top: 10px;
+          width: 100px;
+          }
       }
   }
 </style>
